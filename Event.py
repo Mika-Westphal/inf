139d53b6dd1e959c76883ce4c8ba776964a3ef41
Mika Westphal <wildermika1@gmail.com>
2020-02-28 13:07:25 +0100

    Ja

--- Event.py
+++ Event.py
@@ -1,5 +1,6 @@
 import wx
 import wx.html
+import time
 
 
 class MyHTMLWindow(wx.Frame):
@@ -9,25 +10,35 @@ class MyHTMLWindow(wx.Frame):
 
         wx.InitAllImageHandlers()
 
-        bSizerMain = wx.BoxSizer(wx.VERTICAL)
+        self.counter = 0
 
-        html = wx.html.HtmlWindow(self)
-        html.SetRelatedFrame(self, "%s")
-        # bSizerMain.Add(html, 0, wx.ALL, 1)
-        # html.LoadPage("http://info.cern.ch/hypertext/WWW/TheProject.html")
-        html.SetPage('<html><body><img src="img1.jpg" /></body></html>')
+        self.html = wx.html.HtmlWindow(self)
+        self.html.SetRelatedFrame(self, "%s")
+        self.html.SetPage('<html><body><img src="img2.jpg" /></body></html>')
 
-        self.Bind(wx.html.EVT_HTML_LINK_CLICKED, self.OnLinkClicked, html)
-        self.Bind(wx.html.EVT_HTML_CELL_CLICKED, self.OnCellClicked, html)
+        self.Bind(wx.html.EVT_HTML_LINK_CLICKED, self.OnLinkClicked, self.html)
+        self.Bind(wx.html.EVT_HTML_CELL_CLICKED, self.OnCellClicked, self.html)
+        self.Bind(wx.html.EVT_HTML_CELL_HOVER, self.OnCellHover, self.html)
 
     def OnLinkClicked(self, event):
-        print("Ja")
+        dialog = wx.MessageDialog(self, "Link wird geladen!", "", wx.OK)
+        dialog.ShowModal()
+        dialog.Destroy()
+        event.Skip()
+
+    def OnCellHover(self, event):
+        print(f"{self.counter} I'm different!")
+        if self.counter == 200:
+            self.Unbind(wx.html.EVT_HTML_CELL_HOVER, self.html)
+            self.html.SetPage('<html><body><img src="img1.jpg" /><br><a href="http://info.cern.ch/hypertext/WWW/TheProject.html">Go to info.cern.ch</a></body></html>')
+        self.counter += 1
         event.Skip()
 
     def OnCellClicked(self, event):
         dialog = wx.MessageDialog(self, "The cake is a lie!", "", wx.OK)
         dialog.ShowModal()
         dialog.Destroy()
+        self.Unbind(wx.html.EVT_HTML_CELL_CLICKED, self.html)
         event.Skip()
 
 if __name__ == "__main__":
---
+++ Styles.py
@@ -0,0 +1,24 @@
+import wx
+import wx.html
+
+
+class MyHTMLWindow(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id = wx.ID_ANY, title = "Style Example", pos = wx.DefaultPosition,
+                          size = wx.Size(500, 300), style = wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+
+        wx.InitAllImageHandlers()
+
+        bSizerMain = wx.BoxSizer(wx.VERTICAL)
+
+        html = wx.html.HtmlWindow(self, style=wx.html.HW_NO_SELECTION)
+        html.SetRelatedFrame(self, "%s")
+        # bSizerMain.Add(html, 0, wx.ALL, 1)
+        html.LoadPage("http://info.cern.ch/hypertext/WWW/TheProject.html")
+
+
+if __name__ == "__main__":
+    app = wx.App()
+    frame = MyHTMLWindow(None)
+    frame.Show()
+    app.MainLoop()
